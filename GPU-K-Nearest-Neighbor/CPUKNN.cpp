a71e3cf8e4ec9efd8f1e563eb7a6da1b13787427
vishal929 <vishal.patel929@gmail.com>
2021-12-28 15:55:28 -0500

    Add project files.

---
+++ GPU-K-Nearest-Neighbor/CPUKNN.cpp
@@ -0,0 +1,11 @@
+// this file will house logic for cpu k-nn search methodologies, namely naive knn, knn with kd trees, and a small worlds approach to k-nn search
+
+
+typedef struct comparisonElement {
+
+};
+
+// naive KNN compares every element in the dataset to the 
+comparisonElement* naiveKNN() {
+
+}
---
+++ GPU-K-Nearest-Neighbor/dataStructures.cpp
@@ -0,0 +1,16 @@
+// file holds all the logic for data structures to use for k-nn (mostly for cpu side)
+
+// need a max heap to find elements of LARGEST distance away, so they can be thrown away, and we need a min heap to find the LOWEST distance best neighbors
+class priorityQueue {
+	public:
+		// heaps have a size	
+		int size = 0;
+		// internal implementation will be an array, of fixed size
+	
+
+};
+
+struct priorityQueue {
+	int size = 0;
+
+};
