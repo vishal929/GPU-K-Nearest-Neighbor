67fc508e421e9cf9de4189f6906fda3987f5cb4a
vishal929 <vishal.patel929@gmail.com>
2022-01-05 16:57:08 -0500

    refactored and organized priority queue logic

---
+++ GPU-K-Nearest-Neighbor/myPriorityQueue.cpp
@@ -0,0 +1,113 @@
+// file holds all the logic for data structures to use for k-nn when executing cpu code
+#include "myPriorityQueue.h"
+
+template<typename K>
+void PriorityQueue<K>::siftDown(int position) {
+	// comparisons with children, if appropriate
+			int leftChild = 2 * position +1;
+			int rightChild = leftChild + 1;
+			// best is defined based on our comparison type
+			int bestIndex = position;
+			if (leftChild <= this->getSize() - 1) {
+				// this is a valid comparison index
+				if (this->comparisonType) {
+					// then children should be larger than the parent
+					(this->heapArray[leftChild].second.value < heapArray[position].second.value) ? bestIndex = leftChild : ;
+				}
+				else {
+					// then children should be smaller than the parent
+					(heapArray[leftChild].second.value > heapArray[position].second.value) ? bestIndex = leftChild : ;
+				}
+			}
+			if (rightChild <= this->getSize() - 1) {
+				// this is a valid comparison index
+				if (this->comparisonType) {
+					// then children should be larger than the parent
+					(heapArray[rightChild].second.value < heapArray[position].second.value) ? bestIndex = rightChild : ;
+				}
+				else {
+					// then children should be smaller than the parent
+					(heapArray[rightChild].second.value > heapArray[position].second.value) ? bestIndex = rightChild : ;
+				}
+			}
+			if (bestIndex != position) {
+				// then we have to continue the siftDown operation after swapping
+				pair<void*, K> temp = this->heapArray[position];;
+				this->heapArray[position] = this->heapArray[bestIndex];
+				this->heapArray[bestIndex] = temp;
+				this->siftDown(bestIndex);
+			}
+}
+
+// sift up operation on a specific index
+template<typename K>
+void PriorityQueue<K>::siftUp(int position) {
+	if (position == 0) return;
+	// getting parent index
+	int parent = (position+1) / 2;
+	if (this->comparisonType) {
+		// then the parent should be <= the child
+		if (this->heapArray[parent].second.value > this.heapArray[position].second.value) {
+			// need to swap and continue sift up operation
+			pair<void*, K> temp = this->heapArray[position];
+			this->heapArray[position] = this->heapArray[parent];
+			this->heapArray[parent] = temp;
+			this->siftUp(parent);
+		}
+	}
+	else {
+		// then the parent should be >= the child
+		if (this->heapArray[parent].second.value < this.heapArray[position].second.value) {
+			// need to swap and continue sift up operation
+			pair<void*, K> temp = this->heapArray[position];
+			this->heapArray[position] = this->heapArray[parent];
+			this->heapArray[parent] = temp;
+			this->siftUp(parent);
+		}
+	}
+}
+
+// peeking the top element
+template <typename K>
+pair<void*, K> PriorityQueue<K>::peek() {
+	if (this->getSize() == 0) return NULL;
+	return this->heapArray[0];
+}
+
+// extracting the top element
+template<typename K>
+pair<void*, K> PriorityQueue<K>::extractTop() {
+	if (this->getSize() == 0) return NULL;
+	pair < void*, K> top = this->heapArray[0];
+	// swapping top value with a leaf value, if possible
+	if (this->getSize() == 1) {
+		this->heapArray.clear()
+	}
+	else {
+		this->heapArray[0] = this->heapArray[(this->heapArray).size() - 1];
+		// removing last element
+		(this->heapArray).delete((this->heapArray).end());
+		// performing the sift down operation on the root
+		this->siftdown(0);
+	}
+
+	return top;
+}
+
+// inserting elements
+template<typename K>
+void PriorityQueue<K>::insertElement(pair<void*,K> element) {
+	this->heapArray.push_back(element);
+	this->siftUp(this->heapArray.size() - 1);
+}
+
+// heapify operation given a vector of tuples representing the data and their key values
+template<typename K>
+void PriorityQueue<K>::heapify(vector<pair<void*,K>> elementKeyArray) {
+	// we keep inserting elements as the last element and sifting down
+	for (int i = 0;i < elementKeyArray.size();i++) {
+		this->heapArray.push_back(element);
+		this->siftdown(this->heapArray.size() - 1);
+	}
+}
+
---
+++ GPU-K-Nearest-Neighbor/myPriorityQueue.h
@@ -0,0 +1,56 @@
+#pragma once
+#include <stdlib.h>
+#include <vector>
+#include <any>
+using namespace std;
+
+// K is the class for the key
+// elements are void*  (these are not relevant to the priority queue until data extraction, so I will keep them as void*, we do not want to copy tuple objects anyway)
+template<typename K>
+class PriorityQueue {
+	public:
+		// our heaps will have a maximum size (for bounding knn)
+		int maxSize;
+		// vector of tuple representing the element and a double key
+		*vector< pair<void*, K>> heapArray;
+		// need to specify whether this is a maxHeap or a minHeap
+		bool comparisonType;
+		/*Function declarations*/
+		// false for min heap, true for max heap
+		//constructor
+		PriorityQueue<K> PriorityQueue(int maxSize, bool comparisonType) {
+			this->maxSize = 0;
+			this->comparisonType = comparisonType;
+			//smart pointer for the heap vector
+			this->heapArray = ptr(new vector<pair<void*, K>>());
+		}
+		
+		// getting the size of the heapArray currently
+		int getSize() {
+			return this->heapArray->size();
+		}
+
+		// returns 1 if empty, 0 if not empty
+		bool isEmpty() {
+			return this->getSize() == 0;
+		}
+
+		//sift down operation on a specific index
+		void siftDown(int position);
+
+		// sift up operation on a specific index
+		void siftUp(int position);
+
+		// peeking the top element
+		pair<void*, K> peek();
+
+		// extracting the top element
+		pair<void*, K> extractTop();
+
+		// inserting elements
+		void insertElement(pair<void*, K> element);
+
+		// heapify operation given a vector of tuples representing the data and their key values
+		void heapify(vector<pair<void*, K>> elementKeyArray);
+
+};
